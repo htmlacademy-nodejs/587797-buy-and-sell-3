e3bdd3362ab6234e26cbe431e42bc95cff7dec14
Shv-sergey70 <shv.sergey70@gmail.com>
2020-01-28 17:41:27 +0300

    added files with data

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -13,49 +13,13 @@ const {
   ExitCode
 } = require(`../../constants`);
 
+const FILE_SENTENCES_PATH = `./src/service/data/sentences.txt`;
+const FILE_TITLES_PATH = `./src/service/data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./src/service/data/categories.txt`;
+
 const fs = require(`fs`).promises;
 
-const TITLES = [
-  `Продам книги Стивена Кинга.`,
-  `Продам новую приставку Sony Playstation 5.`,
-  `Продам отличную подборку фильмов на VHS.`,
-  `Куплю антиквариат.`,
-  `Куплю породистого кота.`,
-  `Продам коллекцию журналов «Огонёк».`,
-  `Отдам в хорошие руки подшивку «Мурзилка».`,
-  `Продам советскую посуду. Почти не разбита.`,
-  `Куплю детские санки.`
-];
-
-const Sentences = {
-  MAX_VALUE: 5,
-  VALUES: [
-    `Товар в отличном состоянии.`,
-    `Пользовались бережно и только по большим праздникам.`,
-    `Продаю с болью в сердце...`,
-    `Бонусом отдам все аксессуары.`,
-    `Даю недельную гарантию.`,
-    `Если товар не понравится — верну всё до последней копейки.`,
-    `Это настоящая находка для коллекционера!`,
-    `Если найдёте дешевле — сброшу цену.`,
-    `Таких предложений больше нет!`,
-    `Две страницы заляпаны свежим кофе.`,
-    `При покупке с меня бесплатная доставка в черте города.`,
-    `Кажется, что это хрупкая вещь.`,
-    `Мой дед не мог её сломать.`,
-    `Кому нужен этот новый телефон, если тут такое...`,
-    `Не пытайтесь торговаться. Цену вещам я знаю.`
-  ]
-};
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`
-];
+const SENTENCES_MAX_VALUE = 5;
 
 const SumRestrict = {
   MAX: 100000,
@@ -72,16 +36,28 @@ const PictureValue = {
   MAX: 16
 };
 
-const generateOffers = (offersNumber) => {
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+
+    return content.split(`\n`);
+  } catch (error) {
+    console.error(chalk.red(error));
+
+    return [];
+  }
+};
+
+const generateOffers = (offersNumber, titles, categories, sentences) => {
   const offerTypes = Object.keys(OfferType);
 
   return Array(offersNumber).fill({}).map(() => ({
     type: OfferType[offerTypes[getRandomInt(0, offerTypes.length - 1)]],
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    description: shuffleArray(Sentences.VALUES).slice(1, Sentences.MAX_VALUE).join(` `),
+    title: titles[getRandomInt(0, titles.length - 1)],
+    description: shuffleArray(sentences).slice(1, SENTENCES_MAX_VALUE).join(` `),
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
     picture: `item${getRandomInt(PictureValue.MIN, PictureValue.MAX)}.jpg`,
-    category: shuffleArray(CATEGORIES).slice(1, getRandomInt(1, CATEGORIES.length - 1))
+    category: shuffleArray(categories).slice(1, getRandomInt(1, categories.length - 1))
   }));
 };
 
@@ -96,8 +72,12 @@ module.exports = {
       process.exit(ExitCode.SUCCESS);
     }
 
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+
     try {
-      await fs.writeFile(MOCK_FILE_PATH, JSON.stringify(generateOffers(offersNumber)));
+      await fs.writeFile(MOCK_FILE_PATH, JSON.stringify(generateOffers(offersNumber, titles, categories, sentences)));
     } catch (error) {
       console.error(chalk.red(`Can't write data to file...`));
       process.exit(ExitCode.FAIL);
