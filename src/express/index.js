c2df91cdb28f255f5311134cc6955471f8a8efd0
Sergey Shershnev <shv.sergey70@gmail.com>
2020-04-07 10:46:57 +0300

    added pug and couple templates

--- src/express/index.js
+++ src/express/index.js
@@ -6,11 +6,20 @@ const PORT = 8080;
 
 const app = express();
 
+app.use(express.static('markup'));
+
+app.set('views', __dirname + `/templates`);
+app.set(`view engine`, `pug`);
+
 app.use('/my', myRouter);
 app.use('/offers', offersRouter);
 
-app.get('/', (req, res) => res.send(`${req.originalUrl}`));
-app.get('/register', (req, res) => res.send(`${req.originalUrl}`));
+app.get('/', (req, res) => {
+  res.render(`index`, {})
+});
+app.get('/register', (req, res) => {
+  res.render(`register`)
+});
 app.get('/login', (req, res) => res.send(`${req.originalUrl}`));
 app.get('/search', (req, res) => res.send(`${req.originalUrl}`));
 
--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -3,7 +3,11 @@ const {Router} = require('express');
 offersRouter = Router();
 
 offersRouter.get('/add', (req, res) => res.send(`${req.originalUrl}`));
-offersRouter.get('/category/:id', (req, res) => res.send(`${req.originalUrl}`));
+offersRouter.get('/category/:id', (req, res) => {
+  res.render(`offers/category`, {
+    isAuth: true
+  })
+});
 offersRouter.get('/edit/:id', (req, res) => res.send(`${req.originalUrl}`));
 offersRouter.get('/:id', (req, res) => res.send(`${req.originalUrl}`));
 
