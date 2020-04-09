b913ce9943a2fdcdcd83ec78746bfad3646bf820
Sergey Shershnev <shv.sergey70@gmail.com>
2020-04-09 10:19:00 +0300

    created directory for main pug templates

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -5,18 +5,18 @@ const {Router} = require(`express`);
 const mainRouter = new Router();
 
 mainRouter.get(`/`, (req, res) => {
-  res.render(`main`, {
+  res.render(`main/main`, {
     tickets: [1]
   });
 });
 mainRouter.get(`/register`, (req, res) => {
-  res.render(`sign-up`);
+  res.render(`main/sign-up`);
 });
 mainRouter.get(`/login`, (req, res) => {
-  res.render(`login`);
+  res.render(`main/login`);
 });
 mainRouter.get(`/search`, (req, res) => {
-  res.render(`search-result`, {
+  res.render(`main/search-result`, {
     isAuth: true,
     results: [1]
   });
