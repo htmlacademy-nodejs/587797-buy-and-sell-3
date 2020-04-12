c4ed002769ffb6a29d1e46545f76e7bdb48323c6
Sergey Shershnev <shv.sergey70@gmail.com>
2020-04-12 18:48:42 +0300

    tried to mock readContent method

--- src/service/routes/offers.e2e.test.js
+++ src/service/routes/offers.e2e.test.js
@@ -1,19 +1,9 @@
 'use strict';
 
-const {readContent} = require(`../../utils`);
-
-// jest.mock(`../../utils`, () => {
-//   return jest.fn().mockImplementation(() => {
-//     return {
-//       readContent: readContentMock
-//     };
-//   });
-// });
-
 const request = require(`supertest`);
-const categoriesRouter = require(`./categories`);
+const offersRouter = require(`./offers`);
 const app = require(`express`)();
-app.use(categoriesRouter);
+app.use(offersRouter);
 
 const {
   HttpCode
@@ -23,6 +13,5 @@ describe(`test categories route`, () => {
   test(`test categories GET method success answer`, async () => {
     const res = await request(app).get(`/`);
     expect(res.statusCode).toBe(HttpCode.OK);
-    expect(readContent).toHaveBeenCalledTimes(1);
   });
 });
