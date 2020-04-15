0ca8d56626a5d7d531d3c8bde26db8b278b265ad
Sergey Shershnev <shv.sergey70@gmail.com>
2020-04-15 11:06:02 +0300

    added test for categories

--- src/__mocks__/utils.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const utils = jest.genMockFromModule(`../utils`);
-
-utils.readContent = jest.fn();
-
-module.exports = utils;
--- src/service/routes/categories.e2e.test.js
+++ src/service/routes/categories.e2e.test.js
@@ -1,28 +1,50 @@
 'use strict';
 
-const {readContent} = require(`../../utils`);
+jest.mock(`../repositories/categoriesRepository`);
 
-// jest.mock(`../../utils`, () => {
-//   return jest.fn().mockImplementation(() => {
-//     return {
-//       readContent: readContentMock
-//     };
-//   });
-// });
+const CategoriesRepository = require(`../repositories/categoriesRepository`);
 
-const request = require(`supertest`);
+let request = require(`supertest`);
+
 const categoriesRouter = require(`./categories`);
-const app = require(`express`)();
-app.use(categoriesRouter);
+const express = require(`express`);
+
+const app = express();
+
+app.use(`/`, categoriesRouter);
 
 const {
-  HttpCode
+  HttpCode,
 } = require(`../../constants`);
 
 describe(`test categories route`, () => {
   test(`test categories GET method success answer`, async () => {
-    const res = await request(app).get(`/`);
+    const getAllMethodResponse = {
+      isSuccess: true,
+      body: {
+        data: [1, 2, 3]
+      }
+    };
+    CategoriesRepository.getAll.mockReturnValue(getAllMethodResponse);
+
+    const res = await request(app).get(`/`).expect(`Content-Type`, /application\/json/);
+
     expect(res.statusCode).toBe(HttpCode.OK);
-    expect(readContent).toHaveBeenCalledTimes(1);
+    expect(res.body).toStrictEqual(getAllMethodResponse.body);
+  });
+
+  test(`test categories GET method 404 answer`, async () => {
+    const getAllMethodResponse = {
+      isSuccess: false,
+      body: {
+        message: `Wrong`
+      }
+    };
+    CategoriesRepository.getAll.mockReturnValue(getAllMethodResponse);
+
+    const res = await request(app).get(`/`).expect(`Content-Type`, /text\/html/);
+
+    expect(res.statusCode).toBe(HttpCode.NOT_FOUND);
+    expect(res.text).toBe(getAllMethodResponse.body.message);
   });
 });
